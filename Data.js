cfd4260b570c721d63de6aeaa53bc80cdb9f85f6
Will Timpson <will.timpson@gmail.com>
2017-07-24 18:08:02 -0400

    Generated body!

--- Data.js
+++ Data.js
@@ -1,100 +1,129 @@
-var DataObject = {};
+function _randInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-var GeneratePerson = function() {
+var dataObject = {};
+
+dataObject.generatePerson = function() {
   let person = {
     likes: [],
-    dislikes: []
+    dislikes: [],
+    biography: [],
+    resume: []
   };
-  //let rndInt = getRandomInt(0, DataObject.likeOptions.length);
+
+  // Generate likes and dislikes
   for (let _ = 0; _ < 8; _++) {
     person.likes.push(
-      dataObject.likeOptions[getRandomInt(0, DataObject.likeOptions.length)]
+      dataObject.likeOptions[_randInt(0, dataObject.likeOptions.length)]
     );
   }
   for (let _ = 0; _ < 8; _++) {
     person.dislikes.push(
-      dataObject.likeOptions[getRandomInt(0, DataObject.likeOptions.length)]
+      dataObject.likeOptions[_randInt(0, dataObject.likeOptions.length)]
     );
   }
-  console.log(person);
+
+  // Generate Biography
+  for (let _ = 0; _ < 3; _++) {
+    person.biography.push(
+      dataObject.sentences[_randInt(0, dataObject.sentences.length)]
+    );
+  }
+
+  //Generate
+  for (let _ = 0; _ < 5; _++) {
+    let job = { title: '', body: [] };
+    job.title = dataObject.jobs[_randInt(0, dataObject.jobs.length)];
+    for (let _ = 0; _ < 4; _++) {
+      job.body.push(dataObject.history[_randInt(0, dataObject.history.length)]);
+    }
+    job.body = job.body.join(', ');
+    person.resume.push(job);
+  }
+
+  return person;
 };
 
-DataObject.sentences = [
-  "How was the math test?",
-  "Everyone was busy, so I went to the movie alone.",
-  "She did not cheat on the test, for it was not the right thing to do.",
-  "Hurry!",
-  "Tom got a small piece of pie",
-  "If Purple People Eaters are real… where do they find purple people to eat?",
-  "Wednesday is hump day, but has anyone asked the camel if he’s happy about it?",
-  "Cats are good pets, for they are clean and are not noisy.",
-  "The shooter says goodbye to his love.",
-  "I was very proud of my nickname throughout high school but today- I couldn’t be any different to what my nickname was."
+dataObject.sentences = [
+  'How was the math test?',
+  'Everyone was busy, so I went to the movie alone.',
+  'She did not cheat on the test, for it was not the right thing to do.',
+  'Hurry!',
+  'Tom got a small piece of pie',
+  'If Purple People Eaters are real… where do they find purple people to eat?',
+  'Wednesday is hump day, but has anyone asked the camel if he’s happy about it?',
+  'Cats are good pets, for they are clean and are not noisy.',
+  'The shooter says goodbye to his love.',
+  'I was very proud of my nickname throughout high school but today- I couldn’t be any different to what my nickname was.'
 ];
 
-DataObject.history = [
-  "administered",
-  "analyzed",
-  "appointed",
-  "approved",
-  "assigned",
-  "attained",
-  "authorized",
-  "chaired",
-  "considered",
-  "consolidated",
-  "contracted",
-  "controlled",
-  "converted",
-  "coordinated",
-  "decided",
-  "delegated",
-  "developed",
-  "directed",
-  "eliminated",
-  "emphasized",
-  "enforced",
-  "enhanced",
-  "established",
-  "executed",
-  "generated"
+dataObject.history = [
+  'administered',
+  'analyzed',
+  'appointed',
+  'approved',
+  'assigned',
+  'attained',
+  'authorized',
+  'chaired',
+  'considered',
+  'consolidated',
+  'contracted',
+  'controlled',
+  'converted',
+  'coordinated',
+  'decided',
+  'delegated',
+  'developed',
+  'directed',
+  'eliminated',
+  'emphasized',
+  'enforced',
+  'enhanced',
+  'established',
+  'executed',
+  'generated'
 ];
-DataObject.jobs = [
-  "Baggage handler",
-  "Bailiff",
-  "Baker",
-  "Bank clerk",
-  "Bank manager",
-  "Bar staff",
-  "Barber",
-  "Barrister",
-  "Beauty therapist",
-  "Blacksmith",
-  "Boat builder",
-  "Bodyguard"
+dataObject.jobs = [
+  'Baggage handler',
+  'Bailiff',
+  'Baker',
+  'Bank clerk',
+  'Bank manager',
+  'Bar staff',
+  'Barber',
+  'Barrister',
+  'Beauty therapist',
+  'Blacksmith',
+  'Boat builder',
+  'Bodyguard'
 ];
-DataObject.likeOptions = [
-  "triacetyloleandomycin",
-  "chorioepitheliomata",
-  "overindividualization",
-  "disestablishmentarianism",
-  "magnetothermoelectricity",
-  "antiutilitarianism",
-  "polytetrafluoroethylene",
-  "aerobacteriologically",
-  "bioelectrogenetically",
-  "phenylethylmalonylurea",
-  "interdenominationalism",
-  "cyclotrimethylenetrinitramine",
-  "magnetohydrodynamically",
-  "deoxyribonucleoprotein",
-  "anatomicopathological",
-  "antimaterialistically",
-  "trinitrophenylmethylnitramine",
-  "dicyclopentadienyliron",
-  "poliencephalomyelitis",
-  "overintellectualization",
-  "electrophysiologically",
-  "electrocardiographically"
+dataObject.likeOptions = [
+  'triacetyloleandomycin',
+  'chorioepitheliomata',
+  'overindividualization',
+  'disestablishmentarianism',
+  'magnetothermoelectricity',
+  'antiutilitarianism',
+  'polytetrafluoroethylene',
+  'aerobacteriologically',
+  'bioelectrogenetically',
+  'phenylethylmalonylurea',
+  'interdenominationalism',
+  'cyclotrimethylenetrinitramine',
+  'magnetohydrodynamically',
+  'deoxyribonucleoprotein',
+  'anatomicopathological',
+  'antimaterialistically',
+  'trinitrophenylmethylnitramine',
+  'dicyclopentadienyliron',
+  'poliencephalomyelitis',
+  'overintellectualization',
+  'electrophysiologically',
+  'electrocardiographically'
 ];
-module.exports = DataObject;
+
+module.exports = dataObject;
--- index.js
+++ index.js
@@ -1,67 +1,90 @@
-let express = require("express");
-let uuid = require("uuid/v1");
-let exphbs = require("express-handlebars");
+let express = require('express');
+let uuid = require('uuid/v1');
+let exphbs = require('express-handlebars');
 let app = express();
-var dataObject = require("./Data");
-const port = process.env.PORT || "3000";
+var dataObject = require('./Data');
+const port = process.env.PORT || '3000';
 
 // parsers
-const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 var anythingReally = {};
-var colorArray = ["Green", "Red", "Orange", "Purple", "Yellow", "Blue"];
+var colorArray = ['Green', 'Red', 'Orange', 'Purple', 'Yellow', 'Blue'];
 
 // Templates!
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 // POST middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 // Cookie middleware
 app.use(cookieParser());
+// Static file middleware
+app.use(express.static(`${__dirname}/public`));
 
-app.get("/", (req, res) => {
-  dataObject.GeneratePerson();
+app.get('/', (req, res) => {
   // get status from cookie if it's set or use defaults
-  let theCookie = CheckForCookie(req, res);
-  console.log("this is the get");
-  let formStatus = anythingReally[theCookie] || {
+  let theCookie = getCookie(req, res);
+  // Make sure we have a user object
+  let userObject = anythingReally[theCookie];
+  userObject = userObject || {
     good: true,
-    food: "Soylent Green",
-    color: "Green",
+    food: 'Soylent Green',
+    color: 'Green',
     insanity: 0
   };
-  console.log(formStatus);
-  res.render("index", {
-    status: formStatus,
-    colors: colorArray
+  // Make sure that user has a person
+  if (!userObject.person) {
+    userObject.person = dataObject.generatePerson();
+  }
+
+  // Show the people
+  res.render('index', {
+    status: userObject,
+    colors: colorArray,
+    classes: getClasses(userObject)
   });
 });
 
-app.post("/", (req, res) => {
+app.post('/', (req, res) => {
+  let currentCookie = getCookie(req, res);
+  let person;
+  if (anythingReally[currentCookie]) {
+    person = anythingReally[currentCookie].person;
+  }
   // parse post data
   let formStatus = {
     good: Boolean(Number(req.body.goodbad)),
     food: req.body.food,
     color: req.body.color,
-    insanity: req.body.insanity
+    insanity: req.body.insanity,
+    person: person
   };
+
   // set cookie
-  let currentCookie = CheckForCookie(req, res);
   anythingReally[currentCookie] = formStatus;
-  res.redirect("back");
+  res.redirect('back');
 });
 
 app.listen(port, () => {
-  console.log("Serving!");
+  console.log('Serving!');
 });
-var CheckForCookie = function(requestedFrom, responseFrom) {
+
+function getCookie(requestedFrom, responseFrom) {
   // console.log(anythingReally);
   let currentCookie = requestedFrom.cookies.uuid;
   if (!currentCookie) {
     currentCookie = uuid();
   }
 
-  responseFrom.cookie("uuid", currentCookie);
+  responseFrom.cookie('uuid', currentCookie);
   return currentCookie;
-};
+}
+
+function getClasses(userObject) {
+  let classes = {
+    body: 'insanity-high'
+  };
+
+  return classes;
+}
