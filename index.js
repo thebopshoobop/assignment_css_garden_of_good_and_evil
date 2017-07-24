95c0169b79e17d26721b72aeba84b6600415beae
Will Timpson <will.timpson@gmail.com>
2017-07-24 12:57:35 -0400

    Skeleton

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
-const express = require("express");
+const express = require('express');
+const exphbs = require('express-handlebars');
 const app = express();
-const fs = require("fs");
+const fs = require('fs');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-//app.set("view engine", "hbs");
+const port = process.env.PORT || '3000';
 
-const port = process.env.PORT || "3000";
+// POST middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+// Cookie middleware
+app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  console.log("just a normal get");
-  //res.render("hbsTemplate", variablereplace);
+// Templates!
+app.engine('handebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+  // get status from cookie if it's set or use defaults
+  let status = theCookie || {
+    good: true,
+    food: '',
+    color: '',
+    insanity: 0
+  };
+  // respond  with our template
+  res.render('index', status);
   res.end;
 });
 
+app.post('/', (req, res) => {
+  // parse post data
+  // set cookie
+  // redirect back
+});
+
 app.listen(port);
+
+// Widget List
+// * Radio: good/evil
+// * Text: favorite food
+// * Dropdown: favorite color
+// * Range Slider: insanity level
