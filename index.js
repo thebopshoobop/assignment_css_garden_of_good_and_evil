4cbf96924a2ffd7ed4d000c83d1121631c119a60
Kara Thrash <dvdonato@gmail.com>
2017-07-24 13:19:44 -0400

    nothing new

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
+const express = require("express");
+const exphbs = require("express-handlebars");
 const app = express();
-const fs = require('fs');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
+const fs = require("fs");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 
-const port = process.env.PORT || '3000';
-
+const port = process.env.PORT || "3000";
+var theCookie = false;
 // POST middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 // Cookie middleware
 app.use(cookieParser());
 
 // Templates!
-app.engine('handebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+app.engine("handebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   // get status from cookie if it's set or use defaults
   let status = theCookie || {
     good: true,
-    food: '',
-    color: '',
+    food: "",
+    color: "",
     insanity: 0
   };
   // respond  with our template
-  res.render('index', status);
-  res.end;
+  res.render("index", status);
+  //res.end("index", status);
 });
 
-app.post('/', (req, res) => {
+app.post("/", (req, res) => {
   // parse post data
   // set cookie
   // redirect back
